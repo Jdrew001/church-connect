c165e7a23dc9fe09be555a075a0a582e2757fd95
Drew <dtatkison@gmail.com>
2022-09-30 16:06:31 -0500

    init files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ButtonModule} from 'primeng/button';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CoreModule,
+    ButtonModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
