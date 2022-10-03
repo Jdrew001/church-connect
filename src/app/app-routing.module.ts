1b26ffe5956ffcfd959b891bf26793e9ce0c4458
Drew <dtatkison@gmail.com>
2022-10-03 11:35:21 -0500

    updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: AppComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
